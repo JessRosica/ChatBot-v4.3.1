ef3c01aa93b265ca000bf1c581302b8593a9b36b
JessRosica <129259307+JessRosica@users.noreply.github.com>
2023-04-11 05:56:50 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -10,7 +10,7 @@ export const defaultSetting = {
   model: "gpt-3.5-turbo" as Model
 }
 
-export const defaultMessage = `- 使用本项目请添加QQ群 [ChatBot蒸汽电饭煲](https://jq.qq.com/?_wv=1027&k=fBw3JZal)。 
+export const defaultMessage = `- 使用本项目请添加QQ群 <a href="https://www.example.com">点击这里打开网页</a>。 
 - 本网站已申请法律保护，请勿滥用以及违反中国法律。 
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
